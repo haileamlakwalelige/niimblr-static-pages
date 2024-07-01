d75c37b89b13f61e76e0ba1d08e2faf6df8134ee
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-07-01 10:26:10 +0300

    add footer in the section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import Footer from "./layout/Footer";
 import Navbar from "./layout/Navbar";
 
 export default function App() {
   return (
-   <div className="min-h-screen bg-slate-900 text-white">
+   <div className="min-h-screen bg-slate-900 text-white overflow-x-hidden">
     <Navbar />
+    {/* Your App Here */}
+    <main className="min-h-screen"></main>
+    <Footer />
    </div>
   )
 }
